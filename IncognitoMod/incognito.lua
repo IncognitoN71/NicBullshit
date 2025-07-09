cdbb9ea51ef5ce331457eb8597886f8db5fbfd3e
IncognitoN71 <Nicolasdinh13@gmail.com>
2025-07-08 20:45:18 -0500

    Delete IncognitoMod directory

--- IncognitoMod/incognito.lua
+++
@@ -1,4 +0,0 @@
-assert(SMODS.load_file("src/jokers.lua"))()
-assert(SMODS.load_file("src/assets.lua"))()
-assert(SMODS.load_file("src/texturedeck.lua"))()
-assert(SMODS.load_file("src/rarity.lua"))()
